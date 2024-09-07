4a732d9d69fa00b98d4475ff6ea13138dd6ec29e
cwhitti <112997645+cwhitti@users.noreply.github.com>
2024-09-06 19:03:10 -0700

    Update Receiver.java

--- P1/src/chat/Receiver.java
+++ P1/src/chat/Receiver.java
@@ -1,5 +1,60 @@
+// claire
+
 package chat;
 
-public class Receiver {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import java.net.Socket;
+import java.net.ServerSocket;
+
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+//import java.io.IOException;
+
+public class Receiver extends Thread 
+{
+
+    static ServerSocket receiverSocket = null;
+    static String userName = null;
+
+    // Constructor 
+    public Receiver()
+    {
+        try 
+        {
+            receiverSocket = new ServerSocket(ChatClient.myNodeInfo.getPort());
+            System.out.println("[Receiver.Receiver] receiver socket created, listening on <IMPLEMENT PORT!>")
+        }
+
+        catch( IOException ex ) // change to print
+        {
+            Logger.getLogger(Receiver.class.getName()).log(Level.SEVERE, "ERROR CREATING RECEIVER")
+        }
     
+        System.out.println(ChatClient.myNodeInfo.getName() + " listening on <IMPLEMENT PORT!>" )
+
+    }
+
+    // thread entry point
+    @Override
+    public void run()
+    {
+        // begin server loop
+        while( true )
+        {
+            // listen for connections and accept
+            try
+            {
+                ( new ReceiverWorker( receiverSocket.accept() ) ).start();
+            }
+            catch ( IOException e )
+            {
+                System.err.println("[Receiver.run] Warning: Error accepting client");
+            }
+        }
+    }
 }
