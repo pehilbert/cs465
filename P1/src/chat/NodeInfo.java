8357c697488fb1f6d39a7ec6f7f5e44732fde0f1
Peter Hilbert <pehilbert03@gmail.com>
2024-09-06 19:10:58 -0700

    Fills in chat client and node info

--- P1/src/chat/NodeInfo.java
+++ P1/src/chat/NodeInfo.java
@@ -1,5 +1,37 @@
 package chat;
 
 public class NodeInfo {
-    
+    private String ip;
+    private int port;
+    private String name;
+
+    // Constructor
+    public NodeInfo(String inIp, int inPort, String inName) 
+    {
+        ip = inIp;
+        port = inPort;
+        name = inName;
+    }
+
+    public NodeInfo(String inIp, int inPort)
+    {
+        ip = inIp;
+        port = inPort;
+    }
+
+    // Getter methods
+    public String getIp() 
+    {
+        return ip;
+    }
+
+    public int getPort() 
+    {
+        return port;
+    }
+
+    public String getName() 
+    {
+        return name;
+    }
 }
